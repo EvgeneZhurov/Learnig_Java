ae492d1629c379ea731af854d62f1d4dbced6216
EvgeneZhurov <zhurovev@yandex.ru>
2022-11-20 13:02:33 +0300

    решение задачи из книги Хорстман стр 204

--- src/main/java/Lesson11.java
+++ src/main/java/Lesson11.java
@@ -1,5 +1,6 @@
 import org.w3c.dom.ls.LSOutput;
 
+import javax.sound.midi.Soundbank;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,17 +10,30 @@ import java.io.FileReader;
 
 public class Lesson11 {
     public static void main(String[] args) throws IOException {
+
         try {
-            System.out.println(readValues());
-        }catch (IOException e) {
+            System.out.println("Решение 1 задачи:");
+            System.out.println(readValues("src/main/resources/Lesson1"));
+        } catch (IOException e) {
             System.out.println("Файл не найден");
         }
+
+        try {
+            System.out.println("Решение 2 задачи:");
+            System.out.println(sumOfValues("src/main/resources/Lesson1")); // Решение 2 задачи
+        } catch (IOException e) {
+            System.out.println("Cумма ывходит за пределы значнеий Double");
+        }
+
+        // 3 задача
+        souT("src/main/resources/Lesson1");
+
     }
 
-    public static List<Double> readValues() throws IOException {
+    public static List<Double> readValues(String filename) throws IOException {
 
         List<Double> array = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Lesson10"));
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
         String str = "";
         try {
             while ((str = reader.readLine()) != null) {
@@ -32,8 +46,27 @@ public class Lesson11 {
         } finally {
             reader.close();
         }
-
-
         return array;
+    }
+
+    public static double sumOfValues(String filename) throws IOException {
+
+        List<Double> array = new ArrayList<>(readValues(filename));
+        double sum = 0;
+        for (int i = 0; i < array.size(); i++) {
+            sum += array.get(i);
+        }
+        if (sum == Double.MAX_VALUE || sum == Double.MIN_VALUE) {
+            throw new IOException(String.format("сумма ывходит за пределы значнеий Double"));
+        }
+        return sum;
+    }
+
+    public static void souT(String filename) {
+        try {
+            System.out.println("Решение 3 задачи: " + sumOfValues(filename));
+        } catch (IOException e) {
+            System.out.println("Ловим на экран любые исключения");
+        }
     }
 }
