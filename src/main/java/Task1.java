1a854e34b81b9e3af15741c2e72e00679e1b6e78
EvgeneZhurov <zhurovev@yandex.ru>
2022-11-22 22:13:18 +0300

    Рекурсивный метод заполнения массива простыми числами

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,41 +1,34 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        int i = 1;
+        int num = 2;
+        int sum = 2;
+        array[0] = 2;
+        while (i < 10) {
+            if (isPrime(num, num - 1)) {
+                array[i] = num;
+                i++;
+                sum += num;
+            }
+            num++;
+        }
 
-/*
-        byte num1 = -128;
-        short num2 = Short.MAX_VALUE;
-        int num3 = Integer.MAX_VALUE;
-        long num4 = Long.MAX_VALUE;
-        boolean isTrue = false;
-        char element ='a';
-        float num5 = 1/3f;
-        double num6 = 1/3d;
-        num4++;
-*/
-/*
+        System.out.println(Arrays.toString(array));
+        System.out.println(sum);
+    }
 
-        List<Integer> array1 = new ArrayList();
-        array1.add(1);
-        array1.add(2);
-        array1.add(3);
-        array1.add(5);
-        array1.add(4);
-        //    System.out.println(Collections.unmodifiableList(array1));
-        int a = array1.size();
-        System.out.println(a + 1);
-        System.out.println("Размер массива array1 :" + " " + a);
-*/
-        List<Integer> array = new ArrayList<>();
+    // Метод проверяет, является ли число простым. Если да, то true.
+    public static boolean isPrime(int num1, int num2) {
 
-        array.add(1);
-        array.add(2);
-        array.add(3);
-        array.add(4);
-
-        System.out.println(Collections.unmodifiableList(array));
+        if (num1 % num2 == 0) {
+            return false;
+        }
+        if (num2 == 2) {
+            return true;
+        }
+        return isPrime(num1, num2 - 1);
     }
 }
