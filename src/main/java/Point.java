4de1692d4758b96ccdf5559a8713d3cbb7e94c01
EvgeneZhurov <zhurovev@yandex.ru>
2022-12-13 07:26:16 +0300

    Програма Урок 2 Хрстман

---
+++ src/main/java/Point.java
@@ -0,0 +1,37 @@
+public final class Point {
+
+    private final double x;
+    private final double y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point translate(double x, double y) {
+        return new Point(this.x + x, this.y + y);
+    }
+
+    public Point scale(double scale) {
+        return new Point(x * scale, y * scale);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(3, 4).translate(1, 1).scale(2);
+        System.out.println(p.getX() + " " + (p.getY()));
+    }
+}
+
