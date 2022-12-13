91661e1cb14ec883f50a943f8116abc333d2bcde
EvgeneZhurov <zhurovev@yandex.ru>
2022-12-13 21:06:25 +0300

    Задача про корову

--- src/main/java/Lesson20.java
+++ src/main/java/Lesson20.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 public class Lesson20 {
     public static void main(String[] args) {
@@ -8,23 +8,8 @@ public class Lesson20 {
         Set<String> array = new TreeSet<>();
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine();
-        StringBuilder str2 = new StringBuilder("");
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) != ' ') {
-                str2.append(str1.charAt(i));
-                continue;
-            }
-            if (str2.length() > 0) {
-                String str = String.valueOf(str2);
-                array.add(str);
-                str2.delete(0, str2.length());
-            }
-        }
-        if (!str2.isEmpty()) {
-
-            String str = String.valueOf(str2);
-            array.add(str);
-        }
+        String[] array2 = str1.strip().toLowerCase().split(" ");
+        Collections.addAll(array, array2);
         System.out.println(array.size());
     }
 }
