814dbbd00a65baefef60ac5920703eb06208960a
EvgeneZhurov <zhurovev@yandex.ru>
2022-12-16 15:17:24 +0300

    Хорстман, задача 9. стр. 107

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car toyotaLC200 = new Car();
+        Car toyotaLC200 = new Car(15);
         System.out.println();
         toyotaLC200.carInfo();
         toyotaLC200.setAmountOfGasolineInTank(100);
         toyotaLC200.setAmountOfGasolineInTank(50);
         toyotaLC200.carInfo();
-        toyotaLC200.setCoordinateX(500);
+        toyotaLC200.moveXMiles(500);
         toyotaLC200.carInfo();
-        toyotaLC200.setCoordinateX(-60);
+        toyotaLC200.moveXMiles(-52);
         toyotaLC200.carInfo();
         System.out.println("Пробег: " + toyotaLC200.getMileage() + " км.");
+        toyotaLC200.moveXMiles(-6000);
+        toyotaLC200.carInfo();
+
     }
 }
