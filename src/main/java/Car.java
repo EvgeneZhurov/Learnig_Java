6327cd855e04ce6e115dabfc0379bceee67f1621
EvgeneZhurov <zhurovev@yandex.ru>
2022-12-21 20:10:06 +0300

    Хорстман, задача 9. стр. 107

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -6,55 +6,55 @@ public final class Car {
     private final int fuelConsumption;        //Расход топлива
     private final int fullTank;               //Емкость бака
 
-    public Car(int fuelConsumption) {
-
+    public Car(int fuelConsumption, int fullTank) {
         this.amountOfGasolineInTank = 10;
         this.coordinateX = 0;
         this.mileage = 0;
         this.fuelConsumption = fuelConsumption;
-        this.fullTank = 100;
+        this.fullTank = fullTank;
     }
 
-    public void carInfo() {
-        System.out.printf("Количество бензина в баке (литры): %.0f, Пробег (км): %d,Координана X: %d \n", amountOfGasolineInTank, mileage, coordinateX);
-    }
-
-    //Метод для передвижения на задланное количество километров
-    public void moveXMiles(int x) {
-
-        if (Math.abs(x) / fuelConsumption > (int) amountOfGasolineInTank) {
-            System.out.println("Требуется заправка, автомобиль не может проехать данное растояние.");
-        } else {
-            this.amountOfGasolineInTank -= (double) Math.abs(x) / fuelConsumption;
-            if (this.amountOfGasolineInTank < 5) {
-                System.out.println("Требуется заправка, бак почти пуст");
-            }
-            this.coordinateX += x;
-            this.mileage += Math.abs(x);
-        }
-    }
-
-    //расстояние от начала координат
-    public int getCoordinateX() {
-        return this.coordinateX;
-    }
-
-    //Метод для заполнения топливного бака
-    public void setAmountOfGasolineInTank(int liters) {
-
-        if (this.amountOfGasolineInTank + liters > this.fullTank) {
-            System.out.println("Бак будет переполнен! ");
-        } else {
-            this.amountOfGasolineInTank += liters;
-        }
-    }
-
-    //Литров топлива в баке
     public double getAmountOfGasolineInTank() {
         return this.amountOfGasolineInTank;
     }
 
     public int getMileage() {
-        return this.mileage;
+        return mileage;
+    }
+
+    public int getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void addAmountOfGasolineInTank(int liters) throws ArithmeticException {
+        if (liters < 0) {
+            throw new ArithmeticException("Количество заправляемого топлива не может быть отрицательным!");
+        }
+        if (this.amountOfGasolineInTank + liters > this.fullTank) {
+            System.out.println("Бак будет переполнен! Заправка не была осуществлена.");
+        } else {
+            this.amountOfGasolineInTank += liters;
+        }
+    }
+
+    public void moveXMiles(int x) {
+        if ((double) Math.abs(x) / fuelConsumption > amountOfGasolineInTank) {
+            System.out.println("Требуется заправка, автомобиль не может проехать данное растояние.");
+        } else {
+            this.amountOfGasolineInTank -= (double) Math.abs(x) / fuelConsumption;
+            this.coordinateX += x;
+            this.mileage += Math.abs(x);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "amountOfGasolineInTank=" + amountOfGasolineInTank +
+                ", coordinateX=" + coordinateX +
+                ", mileage=" + mileage +
+                ", fuelConsumption=" + fuelConsumption +
+                ", fullTank=" + fullTank +
+                '}';
     }
 }
