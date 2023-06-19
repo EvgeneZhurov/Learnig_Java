e3e110a4f0e353471de575bd7c2777482fa327ee
EvgeneZhurov <zhurovev@yandex.ru>
2023-06-19 19:51:14 +0300

    задача с литкода: https://leetcode.com/problems/unique-paths/submissions/974908412/

--- src/main/java/leetcode/medium/InputArrayIsSorted.java
+++ src/main/java/leetcode/medium/InputArrayIsSorted.java
@@ -5,29 +5,25 @@ import java.util.*;
 public class InputArrayIsSorted {
     public static void main(String[] args) {
 
-        int[] array = new int[]{2,7,11,15};
+        int[] array = new int[]{2, 7, 11, 15};
         System.out.println(Arrays.toString(twoSum(array, 9)));
     }
 
     public static int[] twoSum(int[] numbers, int target) {
 
-        Set<Integer> collection = new LinkedHashSet<>();
-
-        for (int v : numbers) {
-            collection.add(v);
-        }
-
+        int start = 0;
+        int finish = numbers.length - 1;
         int[] result = new int[2];
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (collection.contains(target - numbers[i])) {
-                result[0] = i;
-                for (int j = numbers.length - 1; j > i; j--) {
-                    if (numbers[j] == target - numbers[i]) {
-                        result[1] = j;
-                        return result;
-                    }
-                }
+        while (start < finish) {
+            if (numbers[start] + numbers[finish] > target) {
+                finish--;
+            } else if (numbers[start] + numbers[finish] < target) {
+                start++;
+            } else {
+                result[0] = start;
+                result[1] = finish;
+                return result;
             }
         }
         return result;
---
+++ src/main/java/leetcode/medium/UniquePaths.java
@@ -0,0 +1,38 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+    public static void main(String[] args) {
+
+        System.out.println(uniquePaths(3, 7));
+
+    }
+
+    public static int uniquePaths(int m, int n) {
+
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < n; i++) {
+            result[0][i] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            result[i][0] = 1;
+        }
+
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+
+                result[i][j] = result[i][j-1] + result[i - 1][j];
+            }
+        }
+
+        return result[m - 1][n - 1];
+    }
+}
