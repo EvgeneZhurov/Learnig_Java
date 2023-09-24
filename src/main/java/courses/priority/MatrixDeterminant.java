13477385758b0361513ec1eac1d67ec85b680d95
EvgeneZhurov <zhurovev@yandex.ru>
2023-09-24 12:26:00 +0300

    Исправление задачи "Размытие"

--- src/main/java/courses/priority/MatrixDeterminant.java
+++ src/main/java/courses/priority/MatrixDeterminant.java
@@ -2,60 +2,60 @@ package courses.priority;
 
 public class MatrixDeterminant {
     public static void main(String[] args) {
-        double[][] array = {
+        double[][] matrix = {
                 {1, 3, 2, 3},
                 {5, 6, 7, -5},
                 {9, 10, 11, 11},
                 {1, 2, 3, 7}
         };
 
-        if (array.length == 0) {
+        if (matrix.length == 0) {
             System.out.println("У пустой матрицы нет определителя.");
-        } else if (!isSquareMatrix(array)) {
+        } else if (!isSquareMatrix(matrix)) {
             System.out.println("Матрица неквадратная. Определитель считается только у квадратных матриц.");
         } else {
-            System.out.println("Определитель матрицы = " + getDeterminant(array));
+            System.out.println("Определитель матрицы = " + getDeterminant(matrix));
         }
     }
 
-    public static double getDeterminant(double[][] inputArray) {
-        if (inputArray.length == 1) {
-            return inputArray[0][0];
+    public static double getDeterminant(double[][] matrix) {
+        if (matrix.length == 1) {
+            return matrix[0][0];
         }
 
-        return getMatrixDeterminant(inputArray);
+        return getMatrixDeterminant(matrix);
     }
 
-    private static double getMatrixDeterminant(double[][] inputArray) {
-        if (inputArray.length == 2) {
-            return inputArray[0][0] * inputArray[1][1] - inputArray[1][0] * inputArray[0][1];
+    private static double getMatrixDeterminant(double[][] matrix) {
+        if (matrix.length == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
         }
 
         double determinant = 0;
 
-        for (int i = 0; i < inputArray.length; i++) {
-            double[][] outputArray = new double[inputArray.length - 1][inputArray.length - 1];
+        for (int i = 0; i < matrix.length; i++) {
+            double[][] subMatrix = new double[matrix.length - 1][matrix.length - 1];
 
-            for (int j = 0; j < inputArray.length - 1; j++) {
-                for (int k = 1; k < inputArray.length; k++) {
+            for (int j = 0; j < matrix.length - 1; j++) {
+                for (int k = 1; k < matrix.length; k++) {
                     if (j >= i) {
-                        outputArray[k - 1][j] = inputArray[k][j + 1];
+                        subMatrix[k - 1][j] = matrix[k][j + 1];
                         continue;
                     }
 
-                    outputArray[k - 1][j] = inputArray[k][j];
+                    subMatrix[k - 1][j] = matrix[k][j];
                 }
             }
 
-            determinant += Math.pow(-1, i) * inputArray[0][i] * getMatrixDeterminant(outputArray);
+            determinant += Math.pow(-1, i) * matrix[0][i] * getMatrixDeterminant(subMatrix);
         }
 
         return determinant;
     }
 
-    public static boolean isSquareMatrix(double[][] array) {
-        for (double[] column : array) {
-            if (column.length != array.length) {
+    public static boolean isSquareMatrix(double[][] matrix) {
+        for (double[] row : matrix) {
+            if (row.length != matrix.length) {
                 return false;
             }
         }
